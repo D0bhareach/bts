e9f498fb92f1839e0f5b186f41f670de5a395172
zxxz <viacheslav.modestov@gmail.com>
2020-07-15 14:30:18 +0300

    Start to create  UserCommand not Finished. Very rough.

--- src/main/java/com/github/zxxz_ru/command/HelpCommand.java
+++ src/main/java/com/github/zxxz_ru/command/HelpCommand.java
@@ -7,25 +7,29 @@ class HelpCommand {
     private final StringBuilder bld =  new StringBuilder();
     private String composeAdvise() {
         bld.append("[-h; --help] print this help;\n\n")
-                .append("<project, task, user> [-a; --all] print all <projects, tasks,users>;\n")
-                .append("<project, task, user> [--delete-id id] delete <project, task, user> by id.\n\t")
-                .append("If id not exists or id argument missing print message and exit.\n\n")
-                .append("user -id {id} print user by id.\n")
-                .append("user --assign-task user id task id.\n\t")
-                .append("If user id missing or  incorrect and if task id missing or incorrect - print message.\n")
-                .append("user --drop-task user id task id.\n\t")
-                .append("If user id missing or  incorrect and if task id missing or incorrect - print message.\n\n")
-                .append("task [-p {id}; --project id] print all tasks for project with id.\n")
-                .append("task [-u {id}; --user-id id] print all tasks for user with id.\n\n")
-                .append("Save or update existing user:\n")
-                .append("user --put firstname lastname role\n\n")
-                .append("Save or update existing task:\n")
-                .append("task --put \n\tthema=\"thema/name\" \n\tproject=project id \n\ttype=\"type of task\"\n\t")
-                .append("description=\"description of task\" \n\tpriority=[normal, high, done, canceled]\n");
+            .append("<project, task, user> [-a; --all]\t\tprint all <projects, tasks,users>;\n")
+            .append("<project, task, user> [-d id; --delete id]\t\tdelete <project, task, user> by id.\n\t")
+            .append("If id not exists or id argument missing print message and exit.\n\n")
+            .append("user -id id\t\tprint user by id.\n")
+            .append("user --assign-task user_id task_id\t\tAssign user new task\n")
+            .append("\tIf user id missing or  incorrect and if task id missing or incorrect - print message.\n")
+            .append("user --drop-task user id task id.\t\tUser drop task.")
+            .append("\tIf user id missing or  incorrect and if task id missing or incorrect - print message.\n\n")
+            .append("task [-p id; --project id]\t\tprint all tasks for project with id.\n")
+            .append("task [-u id; --user-id id]\t\tprint all tasks for user with id.\n\n")
+            .append("Save new or update existing user:\n")
+            .append("user --put \n\tid=user id (optional parameter use for update)\n\tfirstname=user first name")
+            .append("\n\tlastname=user last name\n\trole=user role\n\n")
+            .append("Save new or update existing task:\n")
+            .append("task --put \n\tthema=\"thema/name\" \n\tproject=project id \n\ttype=\"type of task\"\n\t")
+            .append("description=\"description of task\" \n\tpriority=[normal, high, done, canceled]\n")
+            .append("Save new or update existing project:\n")
+            .append("project --put project_name description\n\n");
         return bld.substring(0);
     }
 
     public void printAdvise (){
         System.out.print(composeAdvise());
+        System.exit(0);
     }
 }
--- src/main/java/com/github/zxxz_ru/command/UserCommand.java
+++ src/main/java/com/github/zxxz_ru/command/UserCommand.java
@@ -1,8 +1,76 @@
 package com.github.zxxz_ru.command;
 
+import com.github.zxxz_ru.dao.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import com.github.zxxz_ru.entity.User;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 @Component
 class UserCommand implements Commander {
 
+    @Autowired
+    private UserRepository re;
+    private List<User> getAll(){
+        @SuppressWarnings("unchecked")
+        List result = (List<User>)re.findAll();
+       return result;
+    }
+    private void deleteUser(int id){
+        List<User> list = getUser(id);
+        if(list.size() != 0) {
+            re.delete(list.get(0));
+        }
+    }
+    private List<User> getUser(int id){
+        List<User> res = new ArrayList();
+        @SuppressWarnings("unchecked")
+        Optional<User> opt = re.findById(id);
+        try {
+            res.add(opt.get());
+            return res;
+        } catch (NoSuchElementException e) {
+            return res;
+        }
+    }
+    private void assignTask(int userId, int taskId){}
+    private void dropTask(int userId, int taskId){}
+
+    private String parseSingleParameter(String pair) throws NoSuchElementException{
+    int index = pair.lastIndexOf("=");
+        if(index<0) throw new NoSuchElementException("Incorrect usage [ = ] is missing.");
+        String param = pair.substring(index);
+        if(param.length() == 0) throw new NoSuchElementException("No Parameter after equality Character.");
+        return param;
+    }
+    private boolean idIsPresent(String... args){
+        return Stream.of(args).anyMatch((a)-> Pattern.matches("id(\\=){1}.*", a));
+    }
+
+    // TODO: putUser method not complete totally wrong make new.
+    private List<User> putUser(String... data){
+        List<User> list = new ArrayList();
+        User u;
+        int count = (int)re.count();
+        if(data.length == 4){
+           u = new User(count+1, data[1], data[2], data[3]);
+        } else if(data.length == 3 ){
+            u = new User(count+1, data[1], data[2], "Developer");
+        }
+        return list;
+    }
+
+    @Override
+    public void execute(String... args){
+        String z = args[0];
+        switch(z){
+            case "-a": case "--all":
+                Util.print(getAll());
+
+        }
+        System.exit(0);
+    }
 }
