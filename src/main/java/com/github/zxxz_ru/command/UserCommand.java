4f3a33017dc7eed7f56dd59a42d1db6688d76c78
zxxz <viacheslav.modestov@gmail.com>
2020-08-08 21:47:34 +0300

    2.9 Dispatcher before Generization

--- src/main/java/com/github/zxxz_ru/command/UserCommand.java
+++ src/main/java/com/github/zxxz_ru/command/UserCommand.java
@@ -8,6 +8,7 @@ import com.github.zxxz_ru.entity.User;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
@@ -22,13 +23,13 @@ class UserCommand implements Commander {
     private Messenger messenger;
 
     private List<User> getAll() {
-        return (List<User>)repository.findAll();
+        return (List<User>) repository.findAll();
     }
 
 
     private List<User> getUser(int id) throws NoSuchElementException {
         List<User> res = new ArrayList<>();
-        Optional<User> opt =repository.findById(id);
+        Optional<User> opt = repository.findById(id);
         opt.ifPresent(user -> res.add((User) user));
         return res;
     }
@@ -36,7 +37,7 @@ class UserCommand implements Commander {
     private List<User> deleteUser(int id) {
         List<User> list = getUser(id);
         if (list.size() != 0) {
-           repository.delete(list.get(0));
+            repository.delete(list.get(0));
         }
         return list;
     }
@@ -77,35 +78,36 @@ class UserCommand implements Commander {
     // Update method only one required parameter id.
     // Must get user from Db if not print message that user do not exists.
     // return updated user for reference.
-    private List<User> updateUser(String... args) throws NoSuchElementException{
+    private List<User> updateUser(String... args) throws NoSuchElementException {
         List<User> list = new ArrayList<>();
         User user = new User();
         // no more fun!
         if (!isPresent("id", args)) messenger.printError("user --put id is required parameter.");
         int userId;
         // get userId from parameters or create new.
-            userId = Integer.parseInt(util.getParameter("id", args));
-            Optional<User> userOption =repository.findById(userId);
-            if (userOption.isEmpty()) {
-                messenger.printMessage("No user with id: " + userId);
-            }
-            user = userOption.get();
-            if (isPresent("firstname", args)) {
-                String firstName = util.getParameter("firstname",args);
-                user.setFirstName(firstName);
-            }
-            if (isPresent("lastname", args)) {
-                String lastName = util.getParameter("lastname",args);
-                user.setLastName(lastName);
-            }
-            // Role Parameter not null and will be either default value or value provided in parameters.
-            String role = getRoleParameter(args);
-            user.setRole(role);
-            user = (User) repository.save(user);
-            list.add(user);
+        userId = Integer.parseInt(util.getParameter("id", args));
+        Optional<User> userOption = repository.findById(userId);
+        if (userOption.isEmpty()) {
+            messenger.printMessage("No user with id: " + userId);
+        }
+        user = userOption.get();
+        if (isPresent("firstname", args)) {
+            String firstName = util.getParameter("firstname", args);
+            user.setFirstName(firstName);
+        }
+        if (isPresent("lastname", args)) {
+            String lastName = util.getParameter("lastname", args);
+            user.setLastName(lastName);
+        }
+        // Role Parameter not null and will be either default value or value provided in parameters.
+        String role = getRoleParameter(args);
+        user.setRole(role);
+        user = (User) repository.save(user);
+        list.add(user);
         return list;
     }
-// TODO: When inserting without id id is null!!!!
+
+    // TODO: When inserting without id id is null!!!!
     private List<User> saveUser(String... args) throws NoSuchElementException, IllegalArgumentException {
         List<User> list = new ArrayList<>();
         User user = createNewUser(args);
@@ -116,8 +118,36 @@ class UserCommand implements Commander {
 
     @Override
     public void execute(String args) {
+        String wrongParameter = "Wrong usage! See help. [-h; help]";
+        Pattern pattern = Pattern.compile("(-{1,2}\\w+)(\\s(\\d+))?");
+        Matcher matcher = pattern.matcher(args);
+        String command="";
+        int value=0;
+        if (matcher.find()) {
+            for (int i = 1; i <= 2; i++) {
+                if (i == 1) {
+                    command = matcher.group(1);
+                } else if(i == 2){
+                    try {
+                        String tmp = matcher.group(2);
+                        tmp = tmp != null? tmp.trim(): "No integer!"; // do not pass null to parser
+                        value = Integer.parseInt(tmp);
+                    }catch (NumberFormatException e){
+                        // do not want to fall will ask user to check input.
+                    }
+                    if(value == 0){
+                        messenger.printError(wrongParameter);
+                        messenger.printError("Check parameter's value");
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("Command: "+command+". Value: "+value);
+        // Check if command is id send input to handleIdCommand(String str)
+        // If command is update send input to handleUpdate(String str)
+
         /* String z = args[0];
-        String wrongParameter = "Wrong usage! See help. [-h; --help]";
         switch (z) {
             case "-a":
             case "--all":
